39a55c4ffd4d9585da457ee880da75906b1b7a87
Edwards Moses <edwardsmoses3@gmail.com>
2021-09-24 08:47:50 +0100

    Completed Extra Credit

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,18 +4,6 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle({children}) {
-  const [on, setOn] = React.useState(false)
-  const toggle = () => setOn(!on)
-
-  return React.Children.map(children, child => {
-    return React.cloneElement(child, {
-      on,
-      toggle,
-    })
-  })
-}
-
 // Accepts `on` and `children` props and returns `children` if `on` is true
 const ToggleOn = ({on, children}) => (on ? children : null)
 
@@ -25,12 +13,30 @@ const ToggleOff = ({on, children}) => (on ? null : children)
 // Accepts `on` and `toggle` props and returns the <Switch /> with those props.
 const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
 
+const allowedTypes = [ToggleOn, ToggleOff, ToggleButton]
+
+function Toggle({children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return React.Children.map(children, child => {
+    if (allowedTypes.includes(child.type)) {
+      return React.cloneElement(child, {
+        on,
+        toggle,
+      })
+    }
+    return child
+  })
+}
+
 function App() {
   return (
     <div>
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <p>Hello</p>
         <ToggleButton />
       </Toggle>
     </div>
